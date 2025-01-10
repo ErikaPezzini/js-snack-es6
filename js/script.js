7d8436aa188757e144ff7cc8f91fcc5caeca9274
ErikaPezzini <erikapezzini66@gmail.com>
2025-01-10 13:21:31 +0100

    feat: array 10 auto

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Crea un array di 10 automobili
+
+let automobili = [
+    { marca: 'Fiat', modello: 'Punto', alimentazione: 'benzina' },
+    { marca: 'Volkswagen', modello: 'Golf', alimentazione: 'diesel' },
+    { marca: 'Tesla', modello: 'Model 3', alimentazione: 'elettrico' },
+    { marca: 'Ford', modello: 'Focus', alimentazione: 'benzina' },
+    { marca: 'BMW', modello: 'Serie 3', alimentazione: 'diesel' },
+    { marca: 'Chevrolet', modello: 'Spark', alimentazione: 'gpl' },
+    { marca: 'Audi', modello: 'A4', alimentazione: 'diesel' },
+    { marca: 'Toyota', modello: 'Yaris', alimentazione: 'benzina' },
+    { marca: 'Nissan', modello: 'Leaf', alimentazione: 'elettrico' },
+    { marca: 'Fiat', modello: '500', alimentazione: 'metano' },
+  ];
+
+  
